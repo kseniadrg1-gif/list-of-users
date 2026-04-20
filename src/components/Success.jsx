02a6512935f5830ea8ec0db1a91e48b85ab6ced3
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-04-20 13:20:42 +0300

    3

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,13 +3,10 @@ import React from "react";
 export const Success = ({ count, onClickBack }) => {
   return (
     <div className="success-block">
-      <img src="/public/success.svg" alt="Success" />
+      <img src="/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button
-        onClick={() => window.location.reload()}
-        className="send-invite-btn"
-      >
+      <button onClick={onClickBack} className="send-invite-btn">
         Назад
       </button>
     </div>
